ad273003d4691440021e1535c381d30958c7ba60
handyman0 <luiz.number@gmail.com>
2025-11-07 20:46:59 -0300

    implementando a função unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,17 +6,45 @@
 /*   By: lmelo-do <lmelo-do@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 18:37:12 by lmelo-do          #+#    #+#             */
-/*   Updated: 2025/11/04 18:37:32 by lmelo-do         ###   ########.fr       */
+/*   Updated: 2025/11/07 19:03:32 by lmelo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/builtins.h"
 #include "../includes/minishell.h"
 
+static int	is_valid_identifier(char *str)
+{
+	int i = 0;
+
+	if (!str || !*str || ft_isdigit(str[0]))
+		return (0);
+	while (str[i])
+	{
+		if(!ft_isalnum(str[i]) && str[i] != '_')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	builtin_unset(char **argv, t_shell *shell)
 {
-	(void)argv;
-	(void)shell;
-	printf("minishell: unset: ainda não implementado");
-	return (1);
+	int i = 1;
+	int status = 0;
+
+	while (argv[i])
+	{
+		if (!is_valid_identifier(argv[i]))
+		{
+			ft_putstr_fd("minishell: unset: '", STDERR_FILENO);
+			ft_putstr_fd(argv[i], STDERR_FILENO);
+			ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
+			status = 1;
+		}
+		else
+			env_unset(&shell->env, argv[i]);
+		i++;
+	}
+	return (status);
 }
