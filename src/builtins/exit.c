e8af0ae41e1294c96369790e07a38034e13eb069
handyman0 <luiz.number@gmail.com>
2025-11-07 20:46:26 -0300

    implementando o comando exit e tirando os erros de aparecer varios erros

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,17 +6,59 @@
 /*   By: lmelo-do <lmelo-do@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 18:38:40 by lmelo-do          #+#    #+#             */
-/*   Updated: 2025/11/04 18:39:29 by lmelo-do         ###   ########.fr       */
+/*   Updated: 2025/11/07 18:04:03 by lmelo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/builtins.h"
 #include "../includes/minishell.h"
 
+static int	is_valid_number(char *str)
+{
+	int i = 0;
+
+	if (!str || !*str)
+		return (0);
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 int	builtin_exit(char **argv, t_shell *shell)
 {
-	(void)argv;
+	int	exit_code = 0;
+
+	if (argv[1])
+	{
+		if (!is_valid_number(argv[1]))
+		{
+			ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
+			ft_putstr_fd(argv[1], STDERR_FILENO);
+			ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
+			exit_code = 2;
+		}
+		else if (argv[2])
+		{
+			ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+			return (1);
+		}
+		else
+		{
+			exit_code = ft_atoi(argv[1]) % 256;
+			if (exit_code < 0)
+				exit_code = 256 + exit_code;
+		}
+	}
+	else
+		exit_code = shell->exit_status;
 	shell->running = 0;
-	printf("exit\n");
-	return (0);
+	return (exit_code);
 }
